b07fe638821603efe5069d99a5365c4d7d0b6af0
Mias <812137533@qq.com>
2021-03-31 21:55:04 +0800

    chore:modify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
   },
-  // extends: 'eslint:recommended',
   extends: 'kagura',
   parserOptions: {
     ecmaVersion: 12,
